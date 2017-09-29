d58cf3de8ead99c518c7ca95b9c772257ffb5bfa
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2017-09-29 12:17:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,29 +4,32 @@ using namespace std;
 
 int main()
 {
-    for( string stroka; getline( cin, stroka ); ) {
-        int mass[ 10 ];
-        istringstream stream( stroka );
-        bool failure = false;
-        for( int i = 0; i < 10; ++i ) {
-            if( !( stream >> mass[ i ] ) ) {
-                failure = true;
-                break;
-            }
-        }
-        
-        
-        if( !failure ) {
-            int max = mass[0];
-            for( int i = 0; i < 10; ++i ) {
-                if(max <= mass[i]) max = mass [i];
-            }
-            cout << "Max element: " << max;
-        }
-        else {
-            cout << "An error has occured while reading numbers from line" << endl;
+    string stroka; 
+    getline(cin, stroka); 
+    int mass[ 10 ];
+    
+    istringstream stream(stroka);
+    bool failure = false;
+    for( int i = 0; i < 10; ++i ) {
+        if( !(stream >> mass[i])) {
+        failure = true;
+        break;
         }
     }
+        
+        
+    if( !failure ) {
+     int max = mass[0];
+     for( int i = 0; i < 10; ++i ) {
+      if(max <= mass[i]) max = mass [i];
+     }
+     cout << "Max element: " << max;
+    }
+    else 
+    {
+     cout << "An error has occured while reading numbers from line" << endl;
+    }
+    
     cin.get();
     return 0;
 }
