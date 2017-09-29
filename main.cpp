807835e8dbcfa5ba09c4ca2cb4717a912c2080c7
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2017-09-29 12:06:15 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,32 @@
 #include <iostream>
-#include <string>
+#include <sstream>
 using namespace std;
 
 int main()
 {
-    int massive[10];
-    
-    for (int i = 0; i < 10; i++){
-     cin >> massive[i];
-     }
-
-    int max = massive[0]; 
-    for (int i = 0; i < 10; i++){
-        if(max <= massive[i]) max = massive [i];
+    for( string stroka; getline( cin, stroka ); ) {
+        int mass[ 10 ];
+        istringstream stream( stroka );
+        bool failure = false;
+        for( int i = 0; i < 10; ++i ) {
+            if( !( stream >> mass[ i ] ) ) {
+                failure = true;
+                break;
+            }
+        }
+        
+        
+        if( !failure ) {
+            int max = mass[0];
+            for( int i = 0; i < 10; ++i ) {
+                if(max <= mass[i]) max = mass [i];
+            }
+            cout << "Max element: " << max;
+        }
+        else {
+            cout << "An error has occured while reading numbers from line" << endl;
+        }
     }
- 
-    cout << "Max element: " << max;
- 
     cin.get();
     return 0;
 }
